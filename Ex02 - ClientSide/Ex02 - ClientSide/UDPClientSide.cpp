5b6dda06aa974ea825b562186786276f9278486b
Saar <Saar@SAAR-PC>
2018-08-21 11:30:03 +0300

    Final Version

--- Ex02 - ClientSide/Ex02 - ClientSide/UDPClientSide.cpp
+++ Ex02 - ClientSide/Ex02 - ClientSide/UDPClientSide.cpp
@@ -5,8 +5,17 @@ using namespace std;
 #include <winsock2.h> 
 #include <string.h>
 #include <sstream>
+#include <Windows.h>
 
 #define TIME_PORT	27015
+#define QUIT_CHOICE 0
+#define DELAY_ESTIMATION 4
+#define DELAY_ESTIMATION_REQUESTS 100
+#define MEASURE_RTT 5
+#define MEASURE_RTT_REQUESTS 100
+#define FIRST_CHOICE 1
+#define LAST_CHOICE 11
+#define BUFFER_SIZE 255
 
 bool connectToServer(string i_IPAddress, SOCKET& io_ConnSocket, sockaddr_in& io_Server)
 {
@@ -75,7 +84,7 @@ bool recieveMessageFromServer(char* i_ReciveBuffer, SOCKET& io_ConnSocket, socka
 	int bytesRecv = 0;
 
 	// Gets the server's answer using simple recieve (no need to hold the server's address).
-	bytesRecv = recv(io_ConnSocket, i_ReciveBuffer, 255, 0);
+	bytesRecv = recv(io_ConnSocket, i_ReciveBuffer, BUFFER_SIZE, 0);
 	if (SOCKET_ERROR == bytesRecv)
 	{
 		cout << "Time Client: Error at recv(): " << WSAGetLastError() << endl;
@@ -93,7 +102,7 @@ bool recieveMessageFromServer(char* i_ReciveBuffer, SOCKET& io_ConnSocket, socka
 
 void displayOptionsToUser()
 {
-	cout << "Please enter a choice: \n";
+	cout << "Please enter a number: \n";
 	cout << "0 - Exit! \n";
 	cout << "1 - GetTime \n";
 	cout << "2 - GetTimeWithoutDate \n";
@@ -108,40 +117,164 @@ void displayOptionsToUser()
 	cout << "11 - GetDaylightSavings \n\n";
 }
 
-bool answerUserRequests()
+bool isLegitChoice(const char* i_UserInput, int &o_UserInputNumber)
+{
+	int i = 0, mult = 1, len = strlen(i_UserInput);
+	o_UserInputNumber = 0;
+	bool isNumber = true;
+	bool isInRange = true;
+
+	for (int i = 0; i < len && isNumber; i++)
+	{
+		if (i_UserInput[i] < '0' || i_UserInput[i] > '9')
+		{
+			isNumber = false;
+		}
+		else // Current char is a number.
+		{
+			o_UserInputNumber = o_UserInputNumber*mult + i_UserInput[i] - '0';
+			mult *= 10;
+		}
+	}
+
+	if (o_UserInputNumber < QUIT_CHOICE || o_UserInputNumber > LAST_CHOICE)
+	{
+		isInRange = false;
+	}
+
+	return isInRange && isNumber;
+}
+
+DWORD calculateDelay(DWORD i_Responses[], int i_Length)
+{
+	DWORD average, sum = 0;
+
+	for (int i = 0; i < i_Length - 1; i++)
+	{
+		sum += i_Responses[i + 1] - i_Responses[i];
+	}
+
+	average = sum / (i_Length - 1);
+
+	return average;
+}
+
+bool getClientToServerDelayEstimation(char* io_SendBuff, char* io_RecvBuff, SOCKET &io_ConnSocket, sockaddr_in &io_Server)
+{
+	DWORD responsesFromServer[DELAY_ESTIMATION_REQUESTS];
+
+
+	for (int i = 0; i < DELAY_ESTIMATION_REQUESTS; i++)
+	{
+		if (!sendMessageToServer(io_SendBuff, io_ConnSocket, io_Server))
+		{
+			return false;
+		}
+	}
+
+	for (int i = 0; i < DELAY_ESTIMATION_REQUESTS; i++)
+	{
+		if (!recieveMessageFromServer(io_RecvBuff, io_ConnSocket, io_Server))
+		{
+			return false;
+		}
+
+		responsesFromServer[i] = atoi(io_RecvBuff);
+	}
+
+	cout << "\nClient to server delay estimation: " << calculateDelay(responsesFromServer, DELAY_ESTIMATION_REQUESTS) << " milliseconds\n" << endl;
+
+	return true;
+}
+
+bool measureRTT(char* io_SendBuff, char* io_recvBuff, SOCKET &io_ConnSocket, sockaddr_in &io_Server)
+{
+	DWORD startTime, endTime, deltaTime, delayInMilliseconds = 0;
+
+	for (int i = 0; i < DELAY_ESTIMATION_REQUESTS; i++)
+	{
+		startTime = GetTickCount();
+		if (!sendMessageToServer(io_SendBuff, io_ConnSocket, io_Server))
+		{
+			return false;
+		}
+		if (!recieveMessageFromServer(io_recvBuff, io_ConnSocket, io_Server))
+		{
+			return false;
+		}
+		endTime = GetTickCount();
+
+		deltaTime = endTime - startTime;
+		delayInMilliseconds += deltaTime;
+	}
+
+	delayInMilliseconds /= MEASURE_RTT_REQUESTS;
+	cout << "RTT: " << delayInMilliseconds << " milliseconds\n" << endl;
+
+	return true;
+}
+
+void answerUserRequests()
 {
 	SOCKET connSocket;
 	sockaddr_in server;
-	char sendBuff[255]/* = "What's the time?"*/;
-	char recvBuff[255];
+	int userChoice;
+	char sendBuff[BUFFER_SIZE];
+	char recvBuff[BUFFER_SIZE];
 	bool doesUserWantToRequest = true;
 
 	if (!connectToServer("127.0.0.1", connSocket, server))
 	{
-		return false;
+		return;
 	}
 
 	while(doesUserWantToRequest)
 	{
+
 		displayOptionsToUser();
 		cin >> sendBuff;
-
-		if (strcmp(sendBuff, "0") == 0)
+		if(!isLegitChoice(sendBuff, userChoice))
 		{
-			doesUserWantToRequest = false;
+			cout << "\nYou didn't enter a number in the given range!\n";
 		}
 		else
 		{
-			sendMessageToServer(sendBuff, connSocket, server);
-			recieveMessageFromServer(recvBuff, connSocket, server);
+			if (userChoice == QUIT_CHOICE)
+			{
+				doesUserWantToRequest = false;
+			}
+			else if (userChoice == DELAY_ESTIMATION)
+			{
+				if (!getClientToServerDelayEstimation(sendBuff, recvBuff, connSocket, server))
+				{
+					return;
+				}
+			}
+			else if (userChoice == MEASURE_RTT)
+			{
+				if (!measureRTT(sendBuff, recvBuff, connSocket, server))
+				{
+					return;
+				}
+			}
+			else if (userChoice >= FIRST_CHOICE && userChoice <= LAST_CHOICE)
+			{
+				if (!sendMessageToServer(sendBuff, connSocket, server))
+				{
+					return;
+				}
+
+				if (!recieveMessageFromServer(recvBuff, connSocket, server))
+				{
+					return;
+				}
+			}
 		}
 	}
 
 	// Closing connections and Winsock.
-	cout << "Time Client: Closing Connection.\n";
+	cout << "Time Client: Closing Connection.\n\n\n";
 	closesocket(connSocket);
-
-	return true;
 }
 
 void main()
